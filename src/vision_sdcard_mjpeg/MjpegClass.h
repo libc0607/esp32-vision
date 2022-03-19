712e7ff36b9c0e6bf16c651cb61e59ce0d4fdc55
libc0607 <332559105@qq.com>
2022-03-19 22:57:09 +0800

    sync MjpegClass.h to moononournation/RGB565_video commit f8ebd670

--- src/vision_sdcard_mjpeg/MjpegClass.h
+++ src/vision_sdcard_mjpeg/MjpegClass.h
@@ -1,45 +1,102 @@
 #ifndef _MJPEGCLASS_H_
 #define _MJPEGCLASS_H_
 
-#pragma GCC optimize("O3")
+#define READ_BUFFER_SIZE 1024
+#define MAXOUTPUTSIZE 8
+#define NUMBER_OF_DRAW_BUFFER 4
 
-#define READ_BUFFER_SIZE 2048
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/queue.h>
 
 #include <esp_heap_caps.h>
 #include <FS.h>
-#include <Arduino_TFT.h>
-#include "tjpgdClass.h"
+
+#include <JPEGDEC.h>
+
+typedef struct
+{
+  JPEG_DRAW_CALLBACK *drawFunc;
+} paramDrawTask;
+
+static xQueueHandle xqh = 0;
+static JPEGDRAW jpegdraws[NUMBER_OF_DRAW_BUFFER];
+static int queue_cnt, draw_cnt;
+
+static int queueDrawMCU(JPEGDRAW *pDraw)
+{
+  ++queue_cnt;
+  while ((queue_cnt - draw_cnt) > NUMBER_OF_DRAW_BUFFER)
+  {
+    delay(1);
+  }
+
+  int len = pDraw->iWidth * pDraw->iHeight * 2;
+  JPEGDRAW *j = &jpegdraws[queue_cnt % NUMBER_OF_DRAW_BUFFER];
+  j->x = pDraw->x;
+  j->y = pDraw->y;
+  j->iWidth = pDraw->iWidth;
+  j->iHeight = pDraw->iHeight;
+  memcpy(j->pPixels, pDraw->pPixels, len);
+
+  xQueueSend(xqh, &j, 0);
+  return 1;
+}
+
+static void drawTask(void *arg)
+{
+  paramDrawTask *p = (paramDrawTask *)arg;
+  for (int i = 0; i < NUMBER_OF_DRAW_BUFFER; i++)
+  {
+    jpegdraws[i].pPixels = (uint16_t *)heap_caps_malloc(MAXOUTPUTSIZE * 16 * 16 * 2, MALLOC_CAP_DMA);
+    Serial.printf("#%d draw buffer allocated\n", i);
+  }
+  JPEGDRAW *pDraw;
+  Serial.println("drawTask start");
+  while (xQueueReceive(xqh, &pDraw, portMAX_DELAY))
+  {
+    // Serial.printf("task work: x: %d, y: %d, iWidth: %d, iHeight: %d\r\n", pDraw->x, pDraw->y, pDraw->iWidth, pDraw->iHeight);
+    p->drawFunc(pDraw);
+    // Serial.println("task work done");
+    ++draw_cnt;
+  }
+  vQueueDelete(xqh);
+  Serial.println("drawTask end");
+  vTaskDelete(NULL);
+}
 
 class MjpegClass
 {
 public:
-  bool setup(File input, uint8_t *mjpeg_buf, Arduino_TFT *tft, bool multiTask)
+  bool setup(Stream *input, uint8_t *mjpeg_buf, JPEG_DRAW_CALLBACK *pfnDraw, bool enableMultiTask, bool useBigEndian)
   {
     _input = input;
     _mjpeg_buf = mjpeg_buf;
-    _tft = tft;
-    _multiTask = multiTask;
+    _pfnDraw = pfnDraw;
+    _enableMultiTask = enableMultiTask;
+    _useBigEndian = useBigEndian;
 
-    _tft_width = gfx->width();
-    _tft_height = gfx->height();
+    _mjpeg_buf_offset = 0;
+    _inputindex = 0;
+    _remain = 0;
+
+    queue_cnt = 0;
+    draw_cnt = 0;
 
     if (!_read_buf)
     {
       _read_buf = (uint8_t *)malloc(READ_BUFFER_SIZE);
     }
-    for (int i = 0; i < 2; ++i)
+
+    if (_enableMultiTask)
     {
-      if (!_out_bufs[i])
+      if (!xqh)
       {
-        _out_bufs[i] = (uint8_t *)heap_caps_malloc(_tft_width * 48 * 2, MALLOC_CAP_DMA);
+        TaskHandle_t task;
+        _p.drawFunc = pfnDraw;
+        xqh = xQueueCreate(NUMBER_OF_DRAW_BUFFER, sizeof(JPEGDRAW));
+        xTaskCreatePinnedToCore(drawTask, "drawTask", 1600, &_p, 1, &task, 0);
       }
-    }
-
-    _out_buf = _out_bufs[0];
-
-    if (_multiTask)
-    {
-      _jdec.multitask_begin();
     }
 
     return true;
@@ -49,26 +106,51 @@ public:
   {
     if (_inputindex == 0)
     {
-      _buf_read = _input.read(_read_buf, READ_BUFFER_SIZE);
+      _buf_read = _input->readBytes(_read_buf, READ_BUFFER_SIZE);
       _inputindex += _buf_read;
     }
     _mjpeg_buf_offset = 0;
-    int i = 3;
+    int i = 0;
+    bool found_FFD8 = false;
+    while ((_buf_read > 0) && (!found_FFD8))
+    {
+      i = 0;
+      while ((i < _buf_read) && (!found_FFD8))
+      {
+        if ((_read_buf[i] == 0xFF) && (_read_buf[i + 1] == 0xD8)) // JPEG header
+        {
+          // Serial.printf("Found FFD8 at: %d.\n", i);
+          found_FFD8 = true;
+        }
+        ++i;
+      }
+      if (found_FFD8)
+      {
+        --i;
+      }
+      else
+      {
+        _buf_read = _input->readBytes(_read_buf, READ_BUFFER_SIZE);
+      }
+    }
+    uint8_t *_p = _read_buf + i;
+    _buf_read -= i;
     bool found_FFD9 = false;
     if (_buf_read > 0)
     {
       i = 3;
       while ((_buf_read > 0) && (!found_FFD9))
       {
-        if ((_mjpeg_buf_offset > 0) && (_mjpeg_buf[_mjpeg_buf_offset - 1] == 0xFF) && (_read_buf[0] == 0xD9)) // JPEG trailer
+        if ((_mjpeg_buf_offset > 0) && (_mjpeg_buf[_mjpeg_buf_offset - 1] == 0xFF) && (_p[0] == 0xD9)) // JPEG trailer
         {
+          // Serial.printf("Found FFD9 at: %d.\n", i);
           found_FFD9 = true;
         }
         else
         {
           while ((i < _buf_read) && (!found_FFD9))
           {
-            if ((_read_buf[i] == 0xFF) && (_read_buf[i + 1] == 0xD9)) // JPEG trailer
+            if ((_p[i] == 0xFF) && (_p[i + 1] == 0xD9)) // JPEG trailer
             {
               found_FFD9 = true;
               ++i;
@@ -78,21 +160,23 @@ public:
         }
 
         // Serial.printf("i: %d\n", i);
-        memcpy(_mjpeg_buf + _mjpeg_buf_offset, _read_buf, i);
+        memcpy(_mjpeg_buf + _mjpeg_buf_offset, _p, i);
         _mjpeg_buf_offset += i;
         size_t o = _buf_read - i;
         if (o > 0)
         {
           // Serial.printf("o: %d\n", o);
-          memcpy(_read_buf, _read_buf + i, o);
-          _buf_read = _input.read(_read_buf + o, READ_BUFFER_SIZE - o);
+          memcpy(_read_buf, _p + i, o);
+          _buf_read = _input->readBytes(_read_buf + o, READ_BUFFER_SIZE - o);
+          _p = _read_buf;
           _inputindex += _buf_read;
           _buf_read += o;
           // Serial.printf("_buf_read: %d\n", _buf_read);
         }
         else
         {
-          _buf_read = _input.read(_read_buf, READ_BUFFER_SIZE);
+          _buf_read = _input->readBytes(_read_buf, READ_BUFFER_SIZE);
+          _p = _read_buf;
           _inputindex += _buf_read;
         }
         i = 0;
@@ -106,179 +190,56 @@ public:
     return false;
   }
 
+  int getWidth()
+  {
+    return _jpeg.getWidth();
+  }
+
+  int getHeight()
+  {
+    return _jpeg.getHeight();
+  }
+
   bool drawJpg()
   {
-    _fileindex = 0;
     _remain = _mjpeg_buf_offset;
-    TJpgD::JRESULT jres = _jdec.prepare(jpgRead, this);
-    if (jres != TJpgD::JDR_OK)
-    {
-      Serial.printf("prepare failed! %d\r\n", jres);
-      return false;
-    }
 
-    _out_width = std::min<int32_t>(_jdec.width, _tft_width);
-    _jpg_x = (_tft_width - _jdec.width) >> 1;
-    if (0 > _jpg_x)
+    if (_enableMultiTask)
     {
-      _off_x = -_jpg_x;
-      _jpg_x = 0;
+      _jpeg.openRAM(_mjpeg_buf, _remain, queueDrawMCU);
     }
     else
     {
-      _off_x = 0;
-    }
-    _out_height = std::min<int32_t>(_jdec.height, _tft_height);
-    _jpg_y = (_tft_height - _jdec.height) >> 1;
-    if (0 > _jpg_y)
-    {
-      _off_y = -_jpg_y;
-      _jpg_y = 0;
-    }
-    else
-    {
-      _off_y = 0;
+      _jpeg.openRAM(_mjpeg_buf, _remain, _pfnDraw);
     }
 
-    if (_multiTask)
+    _jpeg.setMaxOutputSize(MAXOUTPUTSIZE);
+    if (_useBigEndian)
     {
-      jres = _jdec.decomp_multitask(jpgWrite16, jpgWriteRow);
+      _jpeg.setPixelType(RGB565_BIG_ENDIAN);
     }
-    else
-    {
-      jres = _jdec.decomp(jpgWrite16, jpgWriteRow);
-    }
+    _jpeg.decode(0, 0, 0);
+    _jpeg.close();
 
-    if (jres != TJpgD::JDR_OK)
-    {
-      Serial.printf("decomp failed! %d\r\n", jres);
-      return false;
-    }
     return true;
   }
 
 private:
-  File _input;
-  uint8_t *_read_buf;
+  Stream *_input;
   uint8_t *_mjpeg_buf;
-  int32_t _mjpeg_buf_offset = 0;
+  JPEG_DRAW_CALLBACK *_pfnDraw;
+  bool _enableMultiTask;
+  bool _useBigEndian;
 
-  Arduino_TFT *_tft;
-  bool _multiTask;
-  uint8_t *_out_bufs[2];
-  uint8_t *_out_buf;
-  TJpgD _jdec;
+  uint8_t *_read_buf;
+  int32_t _mjpeg_buf_offset;
 
-  int32_t _inputindex = 0;
+  JPEGDEC _jpeg;
+  paramDrawTask _p;
+
+  int32_t _inputindex;
   int32_t _buf_read;
-  int32_t _remain = 0;
-  uint32_t _fileindex;
-
-  int32_t _tft_width;
-  int32_t _tft_height;
-  int32_t _out_width;
-  int32_t _out_height;
-  int32_t _off_x;
-  int32_t _off_y;
-  int32_t _jpg_x;
-  int32_t _jpg_y;
-
-  static uint32_t jpgRead(TJpgD *jdec, uint8_t *buf, uint32_t len)
-  {
-    MjpegClass *me = (MjpegClass *)jdec->device;
-    if (len > me->_remain)
-      len = me->_remain;
-    if (buf)
-    {
-      memcpy(buf, (const uint8_t *)me->_mjpeg_buf + me->_fileindex, len);
-    }
-    me->_fileindex += len;
-    me->_remain -= len;
-    return len;
-  }
-
-  // for 16bit color panel
-  static uint32_t jpgWrite16(TJpgD *jdec, void *bitmap, TJpgD::JRECT *rect)
-  {
-    MjpegClass *me = (MjpegClass *)jdec->device;
-
-    uint16_t *dst = (uint16_t *)me->_out_buf;
-
-    uint_fast16_t x = rect->left;
-    uint_fast16_t y = rect->top;
-    uint_fast16_t w = rect->right + 1 - x;
-    uint_fast16_t h = rect->bottom + 1 - y;
-    uint_fast16_t outWidth = me->_out_width;
-    uint_fast16_t outHeight = me->_out_height;
-    uint8_t *src = (uint8_t *)bitmap;
-    uint_fast16_t oL = 0, oR = 0;
-
-    if (rect->right < me->_off_x)
-      return 1;
-    if (x >= (me->_off_x + outWidth))
-      return 1;
-    if (rect->bottom < me->_off_y)
-      return 1;
-    if (y >= (me->_off_y + outHeight))
-      return 1;
-
-    if (me->_off_y > y)
-    {
-      uint_fast16_t linesToSkip = me->_off_y - y;
-      src += linesToSkip * w * 3;
-      h -= linesToSkip;
-    }
-
-    if (me->_off_x > x)
-    {
-      oL = me->_off_x - x;
-    }
-    if (rect->right >= (me->_off_x + outWidth))
-    {
-      oR = (rect->right + 1) - (me->_off_x + outWidth);
-    }
-
-    int_fast16_t line = (w - (oL + oR));
-    dst += oL + x - me->_off_x;
-    src += oL * 3;
-    do
-    {
-      int i = 0;
-      do
-      {
-        uint_fast8_t r8 = src[i * 3 + 0] & 0xF8;
-        uint_fast8_t g8 = src[i * 3 + 1];
-        uint_fast8_t b5 = src[i * 3 + 2] >> 3;
-        r8 |= g8 >> 5;
-        g8 &= 0x1C;
-        b5 = (g8 << 3) + b5;
-        dst[i] = r8 | b5 << 8;
-      } while (++i != line);
-      dst += outWidth;
-      src += w * 3;
-    } while (--h);
-
-    return 1;
-  }
-
-  static uint32_t jpgWriteRow(TJpgD *jdec, uint32_t y, uint32_t h)
-  {
-    static int flip = 0;
-    MjpegClass *me = (MjpegClass *)jdec->device;
-    if (y == 0)
-    {
-      me->_tft->setAddrWindow(me->_jpg_x, me->_jpg_y, jdec->width, jdec->height);
-    }
-
-    me->_tft->startWrite();
-    me->_tft->writeBytes((uint8_t *)me->_out_buf, jdec->width * h * 2);
-    me->_tft->endWrite();
-
-    flip = !flip;
-    me->_out_buf = me->_out_bufs[flip];
-
-    return 1;
-  }
+  int32_t _remain;
 };
 
 #endif // _MJPEGCLASS_H_
